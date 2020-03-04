5f5d6fb64db88b0ba2d44d3647abd5bb99dad708
Caoimhe Ní Chaoimh <projects@oakreef.ie>
2020-03-04 19:24:28 +0000

    dynamically find columns for data

--- mendeleevflower.py
+++ mendeleevflower.py
@@ -22,16 +22,22 @@ d.append(dblock)
 
 with open ('elements.csv') as elementscsv:
     rows = csv.reader(elementscsv)
+    symbolIdx = None
+    atomicNIdx = None
+    massIdx = None
     for i, data in enumerate(rows):
         if i == 0:
+            symbolIdx = data.index("Symbol")
+            atomicNIdx = data.index("Atomic Number")
+            massIdx = data.index("Atomic Mass")
             continue
         x=i
         y=0
         element = svg.Group(transform=f"translate({x*bw},{y*bh})")
         element.append(svg.Rectangle(0,0,bw,bh,stroke_width=1,stroke='black', fill='yellow'))
-        symbol = svg.Text(data[2],10,bw/2,bh/2.5,font_family='sans-serif',text_anchor='middle',fill='black')
-        atomicN = svg.Text(data[0],5,2,bh-5,font_family='sans-serif',fill='black')
-        mass = svg.Text(data[3],5,2,2,font_family='sans-serif',fill='black')
+        symbol = svg.Text(data[symbolIdx],10,bw/2,bh/2.5,font_family='sans-serif',text_anchor='middle',fill='black')
+        atomicN = svg.Text(data[atomicNIdx],5,2,bh-5,font_family='sans-serif',fill='black')
+        mass = svg.Text(data[massIdx],5,2,2,font_family='sans-serif',fill='black')
         element.append(symbol)
         element.append(atomicN)
         element.append(mass)
