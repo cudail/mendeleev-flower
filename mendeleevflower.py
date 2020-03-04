d62938c70fbeca1002ce49e8f34ab2ea90d1755f
Caoimhe Ní Chaoimh <caoimhe@oakreef.ie>
2020-03-04 19:58:33 +0000

    Refactoring

--- mendeleevflower.py
+++ mendeleevflower.py
@@ -22,9 +22,7 @@ d.append(dblock)
 
 with open ('elements.csv') as elementscsv:
     rows = csv.reader(elementscsv)
-    symbolIdx = None
-    atomicNIdx = None
-    massIdx = None
+    symbolIdx, atomicNIdx, massIdx = (None,)*3
     for i, data in enumerate(rows):
         if i == 0:
             symbolIdx = data.index("Symbol")
@@ -40,13 +38,11 @@ with open ('elements.csv') as elementscsv:
             x=group > 2 and group-10 or group
             y=period
             element = svg.Group(transform=f"translate({x*bw},{y*bh})")
-            element.append(svg.Rectangle(0,0,bw,bh,stroke_width=1,stroke='black', fill='yellow'))
-            symbol = svg.Text(data[symbolIdx],10,bw/2,bh/2.5,font_family='sans-serif',text_anchor='middle',fill='black')
-            atomicN = svg.Text(data[atomicNIdx],5,2,bh-5,font_family='sans-serif',fill='black')
-            mass = svg.Text(data[massIdx],5,2,2,font_family='sans-serif',fill='black')
-            element.append(symbol)
-            element.append(atomicN)
-            element.append(mass)
             spblock.append(element)
+
+        element.append(svg.Rectangle(0,0,bw,bh,stroke_width=1,stroke='black', fill='yellow'))
+        element.append(svg.Text(data[symbolIdx],10,bw/2,bh/2.5,font_family='sans-serif',text_anchor='middle',fill='black'))
+        element.append(svg.Text(data[atomicNIdx],5,2,bh-5,font_family='sans-serif',fill='black'))
+        element.append(svg.Text(data[massIdx],5,2,2,font_family='sans-serif',fill='black'))
 
 d.saveSvg('out.svg')
