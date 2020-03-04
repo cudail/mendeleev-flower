55c9e60fb9a67db2f82d31736e0b12277ff66151
Caoimhe Ní Chaoimh <projects@oakreef.ie>
2020-03-04 20:38:25 +0000

    add fblock and dblock

--- mendeleevflower.py
+++ mendeleevflower.py
@@ -13,8 +13,8 @@ bw, bh = 30, 30
 
 
 spblock = svg.Group(transform=f"translate(-400,-400)")
-fblock = svg.Group(transform=f"translate(-400,0)")
-dblock = svg.Group(transform=f"translate(400,-400)")
+fblock = svg.Group(transform=f"translate(0,-400)")
+dblock = svg.Group(transform=f"translate(-400,0)")
 
 d.append(spblock)
 d.append(fblock)
@@ -22,7 +22,6 @@ d.append(dblock)
 
 with open ('elements.csv') as elementscsv:
     rows = csv.reader(elementscsv)
-    symbolIdx, atomicNIdx, massIdx = (None,)*3
     for i, data in enumerate(rows):
         if i == 0:
             symbolIdx = data.index("Symbol")
@@ -39,7 +38,17 @@ with open ('elements.csv') as elementscsv:
             y=period
             element = svg.Group(transform=f"translate({x*bw},{y*bh})")
             spblock.append(element)
-
+        elif group > 0:
+            x=group
+            y=period
+            element = svg.Group(transform=f"translate({x*bw},{y*bh})")
+            fblock.append(element)
+        else:
+            a=int(data[atomicNIdx])
+            x=period==6 and a-57 or a-89
+            y=period
+            element = svg.Group(transform=f"translate({x*bw},{y*bh})")
+            dblock.append(element)
         element.append(svg.Rectangle(0,0,bw,bh,stroke_width=1,stroke='black', fill='yellow'))
         element.append(svg.Text(data[symbolIdx],10,bw/2,bh/2.5,font_family='sans-serif',text_anchor='middle',fill='black'))
         element.append(svg.Text(data[atomicNIdx],5,2,bh-5,font_family='sans-serif',fill='black'))
