a784a5503de0ddd2b10b4837cd1870b6feb47fe0
Caoimhe Ní Chaoimh <projects@oakreef.ie>
2020-03-04 19:51:45 +0000

    drawing "sp block" as a single block

--- mendeleevflower.py
+++ mendeleevflower.py
@@ -33,18 +33,20 @@ with open ('elements.csv') as elementscsv:
             periodIdx = data.index("Period")
             groupIdx = data.index("Group")
             continue
-        x=int(data[groupIdx] or 0)
-        y=int(data[periodIdx] or 0)
-        element = svg.Group(transform=f"translate({x*bw},{y*bh})")
-        element.append(svg.Rectangle(0,0,bw,bh,stroke_width=1,stroke='black', fill='yellow'))
-        symbol = svg.Text(data[symbolIdx],10,bw/2,bh/2.5,font_family='sans-serif',text_anchor='middle',fill='black')
-        atomicN = svg.Text(data[atomicNIdx],5,2,bh-5,font_family='sans-serif',fill='black')
-        mass = svg.Text(data[massIdx],5,2,2,font_family='sans-serif',fill='black')
-        element.append(symbol)
-        element.append(atomicN)
-        element.append(mass)
 
-        spblock.append(element)
-
+        group = int(data[groupIdx] or -1)
+        period = int(data[periodIdx] or -1)
+        if (group>=1 and group<=2) or (group>=13):
+            x=group > 2 and group-10 or group
+            y=period
+            element = svg.Group(transform=f"translate({x*bw},{y*bh})")
+            element.append(svg.Rectangle(0,0,bw,bh,stroke_width=1,stroke='black', fill='yellow'))
+            symbol = svg.Text(data[symbolIdx],10,bw/2,bh/2.5,font_family='sans-serif',text_anchor='middle',fill='black')
+            atomicN = svg.Text(data[atomicNIdx],5,2,bh-5,font_family='sans-serif',fill='black')
+            mass = svg.Text(data[massIdx],5,2,2,font_family='sans-serif',fill='black')
+            element.append(symbol)
+            element.append(atomicN)
+            element.append(mass)
+            spblock.append(element)
 
 d.saveSvg('out.svg')
